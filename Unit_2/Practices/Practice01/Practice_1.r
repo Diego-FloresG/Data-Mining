0123abce937ea96b11906521f13d29809c7f5641
JJimenez2117 <43395765+JJimenez2117@users.noreply.github.com>
2021-11-03 19:14:52 -0700

    Update Practice_1.r

--- Unit_2/Practices/Practice01/Practice_1.r
+++ Unit_2/Practices/Practice01/Practice_1.r
@@ -17,3 +17,26 @@ w + geom_point(size=3) + facet_grid(Origin~.)
 v <- ggplot(cars, aes(x=Horsepower))
 v + geom_histogram(binwidth = 10, aes(fill=Origin),color="Black")
 v + geom_histogram(binwidth = 10, aes(fill=Origin),color="Black") + facet_grid(Origin~., scales="free")
+
+
+#theme
+o <- ggplot(cars, aes(x=Horsepower))
+h <- o + geom_histogram(binwidth = 10, aes(fill=Acceleration), color="Black")
+
+h +
+  xlab("Horsepower (HP)") +
+  ylab("Count") +
+  ggtitle("Distribution") +
+  theme(axis.title.x = element_text(color = "Green", size=30),
+        axis.title.y = element_text(color = "Blue", size=30),
+        axis.text.x = element_text(size = 20),
+        axis.text.y = element_text(size = 20),
+        legend.title = element_text(size = 30),
+        legend.text = element_text(size = 20),
+        legend.position = c(1,1),
+        legend.justification = c(1,1),
+        plot.title = element_text(color = "DarkBlue",
+                                  size = 25,
+                                  family = "Courier")
+        
+h
